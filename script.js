ce833d899e57b5b1d6b29f3a280fb7d43271ee65
SourabhSisodia <sourabhsisodia010@gmail.com>
2022-03-22 15:57:52 +0530

    added local storage functionality

--- script.js
+++ script.js
@@ -35,11 +35,28 @@ function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
     darkMode();
+    localStorage.setItem("theme", "dark");
   } else {
     document.documentElement.setAttribute("data-theme", "light");
     lightMode();
+    localStorage.setItem("theme", "light");
   }
 }
 
 //Event Listener
 toggleSwitch.addEventListener("change", switchTheme);
+
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+  if (currentTheme === "light") {
+    toggleSwitch.checked = false;
+    lightMode();
+  }
+}
